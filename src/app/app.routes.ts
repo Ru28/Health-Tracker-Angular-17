478715ec787e30d09189230c852fb9dbf535c2e7
Rupesh Virani <rupeshvirani01@gmail.com>
2024-07-17 18:24:01 +0530

    design header and set route for form, list and progress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { WorkoutFormComponent } from './components/workout-form/workout-form.component';
+import { WorkoutListComponent } from './components/workout-list/workout-list.component';
+import { WorkoutProgressComponent } from './components/workout-progress/workout-progress.component';
 
 export const routes: Routes = [
-    {path:'',component: WorkoutFormComponent},
+    { path: 'workout-form', component: WorkoutFormComponent },
+    { path: 'workout-list', component: WorkoutListComponent },
+    { path: 'workout-progress', component: WorkoutProgressComponent },
+    { path: '', redirectTo: '/workout-form', pathMatch: 'full' },
 ];
